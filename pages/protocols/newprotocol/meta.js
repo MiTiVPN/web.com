65a6fc054ceacb56f9f29823709e5b1b9d941186
MiTiVPN <mitivpn@gmail.com>
2026-07-20 17:19:35 -0700

    Add files via upload

---
+++ pages/protocols/newprotocol/meta.js
@@ -0,0 +1,16 @@
+module.exports = {
+  outPath: 'protocols/newprotocol/index.html',
+  footer: 'footer-article', // or 'footer' for the full contact footer (use on top-level pages)
+  head: `
+<title>New Protocol | MiTiVPN</title>
+<meta name="description" content="TODO">
+<meta name="robots" content="index, follow">
+<link rel="canonical" href="https://mitivpn.com/protocols/newprotocol/">
+
+<meta property="og:type" content="article">
+<meta property="og:site_name" content="MiTiVPN">
+<meta property="og:title" content="New Protocol | MiTiVPN">
+<meta property="og:description" content="TODO">
+<meta property="og:url" content="https://mitivpn.com/protocols/newprotocol/">
+<meta property="og:locale" content="en_US">`
+};
